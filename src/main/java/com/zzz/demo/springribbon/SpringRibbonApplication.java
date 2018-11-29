dd4ca699beea9cb3698f4aea915bcddf27ac74b9
002864 <zhengyijun@xinhehui.com>
2018-11-29 13:53:20 +0800

    new

---
+++ src/main/java/com/zzz/demo/springribbon/SpringRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.zzz.demo.springribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRibbonApplication.class, args);
+	}
+}
